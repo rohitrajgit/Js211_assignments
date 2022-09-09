703280394b1011a37e44ae2d382d8035dcc7130d
Rohit raj <rohitraj994636@gmail.com>
2022-09-09 12:23:37 +0530

    new file

---
+++ js211_weeklyproject/jspolly.js
@@ -0,0 +1,22 @@
+
+
+
+
+function custom_funtion_map(arr,x){
+
+for(var i=0;i<arr.length;i++){
+   let x=Number(arr[i]);// line no 08 you can put here any type of condition which you want 
+   console.log(x)
+}
+
+
+
+}
+
+
+
+let array=["1","2","3","4","5","6"];
+
+let y=["rohit","mohit","vikas",'rahul']
+
+change(array,y);
